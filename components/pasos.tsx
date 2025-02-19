cb472687e6bd9be2e3878bda9bcb8bb555cec871
Agustin-Monti <153923960+Agustin-Monti@users.noreply.github.com>
2025-02-19 10:44:50 -0300

    Update pasos.tsx

--- components/pasos.tsx
+++ components/pasos.tsx
@@ -2,23 +2,23 @@ const pasos = [
   {
     id: 1,
     imgSrc: "/pasos/paso1.png", // Ruta de la imagen de conversación
-    title: "Dinos que necesitas",
+    title: "Publica Gratis lo que Necesitas",
     description:
-      "Cuéntanos sobre tu necesidad y nosotros lo pondremos a disposición de profesionales recomendados que te ayudarán a hacerlo realidad."
+      "Detalla tu demanda de forma clara y precisa para que las personas interesadas puedan contactarse en forma directa y acercar una propuesta que sea la solución que estás buscando algo relacionado con la busqueda."
   },
   {
     id: 2,
     imgSrc: "/pasos/paso2.png", // Ruta de la imagen de documentos
-    title: "Recibe múltiples propuestas",
+    title: "Búsqueda de posibles candidatos que puedan satisfacer tu demanda.",
     description:
-      "En pocas horas recibirás las propuestas solicitadas. Conversa y evacúa dudas a través de nuestro chat. Compara reputación, experiencia y presupuestos."
+      "Tu demanda será publicada en distintos medios, redes sociales, empresas y prestadores de servicios. Asimismo todos los que accedan a la plataforma podrán encontrar tu demanda y evaluar la posibilidad de satisfacer la misma. De existir interes,realizando un único pago te enviaremos la informacion del representante para que te contactes y ofrezcas tu solución."
   },
   {
     id: 3,
     imgSrc: "/pasos/paso3.png", // Ruta de la imagen de usuario con check
-    title: "Elige",
+    title: "Generación del Negocio",
     description:
-      "Cuando tengas toda la información, contrata de manera directa y abona sin intermediarios ni sobrecostos."
+      "Agregar una persona mas que sería la que ofrece la solución. Una vez confirmado el pago se envía la información del responsable de la demanda para que realicen el contacto directo de y respectiva negociación, detalles, precios, presupuestos, etc. La plataforma solo llevará estadisticas de conformidad entre la partes, no existiendo ningún otro costo adicional."
   }
 ];
 
@@ -26,12 +26,11 @@ export default function Pasos() {
   return (
     <section className="text-center py-20"> {/* Sección más grande con padding */}
       <h2 className="text-3xl font-semibold text-gray-800 mb-6"> {/* Título más grande */}
-        ¿CÓMO FUNCIONA NESECITO<span className="text-blue-700">ESTO!</span>
+        CUAL ES EL FUNCIONAMIENTO DE  "NECESITO<span className="text-blue-700">ESTO!</span>"?
       </h2>
-      <p className="text-blue-700 mt-2">Es muy sencillo.</p>
       <p className="text-gray-600 mb-12"> {/* Más espacio entre párrafos */}
-        Tan solo sigue estos tres pasos y en pocas horas encontrarás necesidades
-        para lo que estás buscando.
+        El funcionamiento es muy simple y contempla 3 etapas, donde el objetivo es dar a conocer tu necesidad y encontrar aquel que tenga la idoneidad y el interes de satisfacer tu necesidad contactándose
+        en forma directa con un costo mínimo.
       </p>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-12 max-w-6xl mx-auto">
         {pasos.map((paso) => (
