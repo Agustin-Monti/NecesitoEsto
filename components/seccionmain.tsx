41a1f11a4aa4f070ce220da94edbf71f824039af
Agustin-Monti <153923960+Agustin-Monti@users.noreply.github.com>
2025-02-24 11:50:32 -0300

    Update seccionmain.tsx

--- components/seccionmain.tsx
+++ components/seccionmain.tsx
@@ -1,39 +1,40 @@
-export default function Seccion (){
-
-return(
-    <div className="flex flex-col justify-center bg-blue-50 items-center text-center p-[100px] text-slate-900 font-bold text-lg md:text-sm h-auto md:h-[600px]">
-        <h1 className="text-3xl md:text-2xl transition-transform transform hover:scale-105">
-        ¡Explora un mundo lleno de oportunidades con el modelo de pago por oferta! 🌟
+export default function Seccion() {
+    return (
+      <div className="flex flex-col justify-center bg-blue-50 items-center pb-80 text-center md:p-[100px] text-slate-900 font-bold text-lg md:text-sm h-auto md:h-[600px]">
+        {/* Título principal */}
+        <h1 className="text-2xl md:text-3xl transition-transform transform hover:scale-105 mt-3 pt-3">
+          ¡Explora un mundo lleno de oportunidades con el modelo de pago por oferta! 🌟
         </h1>
-        <br></br>
-        <br></br>
-        <div className="border border-solid border-slate-950 p-4 w-[380px] lg:w-[850px] md:w-[650px] rounded-xl">
-        <h3 className="text-slate-600">
-        Imagina tener acceso a una variedad de servicios especializados, pagando solo por aquello que realmente te interesa.<br></br> 
-        Este modelo no solo es económico, sino que también pone el control en tus manos, permitiéndote elegir lo que mejor se adapta a tus necesidades.<br></br> 
-
-        Ya sea que busques diseño, desarrollo web, marketing, consultoría o cualquier otro servicio, este sistema abre las puertas a soluciones flexibles y personalizadas.<br></br> 
-        Cada oferta representa una posibilidad de crecimiento, innovación o simplemente la ayuda que necesitas para alcanzar tus metas.
-        </h3>
-        <br></br>
-        <br></br>
-        <h2 className="text-slate-600">
-        💡 Atrévete a explorar. Confía en este enfoque dinámico, donde cada elección es una inversión estratégica y accesible. Porque las grandes oportunidades están a solo una oferta de distancia.
-        </h2>
+        <br />
+        <br />
+  
+        {/* Contenedor de texto */}
+        <div className="border border-solid border-slate-950 p-4 w-full md:w-[650px] lg:w-[850px] rounded-xl">
+          <h3 className="text-slate-600 text-base md:text-lg">
+              Imagina tener acceso a una variedad de Demandas de Bienes, Materiales y servicios especializados, realizando un pago mínimo abriendo las posibilidades de un nuevo negocio.
+            <br />
+              Este modelo no solo es económico, sino que también pone el control en tus manos, permitiéndote elegir la solución mas conveniente dentro de aquellos que ofrezcan la mejor propuesta.
+          </h3>
+          <br />
+            <h3 className="text-slate-600 text-base md:text-lg">
+              Ya sea que busques diseño, desarrollo web, marketing, consultoría o cualquier otro servicio, este sistema abre las puertas a obtener soluciones flexibles y propuestas  personalizadas.
+            </h3>
+          <br />
+          <br />
+            <h3 className="text-slate-600 text-base md:text-lg">
+              Cada oferta representa una posibilidad de crecimiento, innovación o simplemente la ayuda que necesitas para alcanzar tus metas.
+            </h3>
+          <br />
+          <h2 className="text-slate-600 text-base md:text-lg">
+            💡 Atrévete a explorar. Confía en este enfoque dinámico y práctico, donde cada elección es una inversión estratégica y accesible donde las grandes oportunidades están a solo una oferta de distancia.
+          </h2>
         </div>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <h4 className="text-2xl md:text-xl transition-transform transform hover:scale-105">
-        ¡Convierte cada clic en una puerta hacia nuevas posibilidades! 🚀
+        <br />
+        <br />
+        {/* Texto final */}
+        <h4 className="text-xl md:text-2xl transition-transform transform hover:scale-105">
+          ¡Convierte cada clic en una puerta hacia nuevas posibilidades de Negocios. 🚀
         </h4>
-        {/*<img
-            src="/hero.jpg"
-            alt="seccion"
-            width={"440px"}
-            className="h-[200px] w-[300px] hidden md:block bg-gradient-to-r from-purple-500 to-pink-500 flex justify-center items-center text-white text-lg [border-top-left-radius:200px] [border-top-right-radius:50px] [border-bottom-left-radius:90px] [border-bottom-right-radius:250px] rotate-[15deg] shadow-xl"
-        />*/}
-</div>
-)
-}
+      </div>
+    );
+  }
