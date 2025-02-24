10840eb07aafe1995b0e52708adada1518960218
Agustin-Monti <153923960+Agustin-Monti@users.noreply.github.com>
2025-02-24 15:44:46 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import  HeaderWithAuth  from "@/components/HeaderWithAuth";
 import WhatsappButton from "@/components/WhatsappButton";
+import FloatingButton from "@/components/FloatingButton";
+
 import Image from "next/image";
 import "./globals.css";
 
@@ -35,10 +37,14 @@ export default function RootLayout({
             <div>
               {children}
               <WhatsappButton />
+              {/* Botón flotante "Publica tu necesidad" en móvil */}
+              <div className="md:hidden fixed bottom-20 right-4 z-50 mb-12">
+                <FloatingButton />
+              </div>
             </div>
 
-            <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16 bg-black bg-gray-400">
-              <p className="h2 text-white text-xl">Necesito<span className='azul'>!Esto</span> © Copyright - 2025</p>
+            <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16 bg-gray-400">
+              <p className="h2 text-white text-xl">Nesecito<span className='azul'>!Esto</span> © Copyright - 2024</p>
               {/*<ThemeSwitcher />*/}
               <Image src="/logotm2.png" alt="Logo de Necesito Esto!" width={170} height={90} />
             </footer>
