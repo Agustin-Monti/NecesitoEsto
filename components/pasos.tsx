571616d626f8c938e31db1e9760343bc26126fc2
Agustin-Monti <153923960+Agustin-Monti@users.noreply.github.com>
2025-03-14 22:56:26 -0300

    Update pasos.tsx

--- components/pasos.tsx
+++ components/pasos.tsx
@@ -4,21 +4,21 @@ const pasos = [
     imgSrc: "/pasos/paso1.png", // Ruta de la imagen de conversación
     title: "Publica Gratis lo que Necesitas",
     description:
-      "Detalla tu demanda de forma clara y precisa para que las personas interesadas puedan contactarse en forma directa y acercar una propuesta que sea la solución que estás buscando algo relacionado con la busqueda."
+      "Detalla tu demanda de forma clara y precisa para que las personas interesadas puedan contactarse en forma directa y acercar una propuesta que sea la solución que estás buscando."
   },
   {
     id: 2,
     imgSrc: "/pasos/paso2.png", // Ruta de la imagen de documentos
     title: "Búsqueda de posibles candidatos que puedan satisfacer tu demanda.",
     description:
-      "Tu demanda será publicada en distintos medios, redes sociales, empresas y prestadores de servicios. Asimismo todos los que accedan a la plataforma podrán encontrar tu demanda y evaluar la posibilidad de satisfacer la misma. De existir interes,realizando un único pago te enviaremos la informacion del representante para que te contactes y ofrezcas tu solución."
+      "Tu demanda (será publicada) podrá ser encontrada en distintos medios, redes sociales, enviadas a empresas y prestadores de servicios. (Asimismo) Como también  todos los que accedan a la plataforma (podrán encontrar) verán tu demanda y (evaluar)  evaluarán la posibilidad de satisfacer la misma."
   },
   {
     id: 3,
     imgSrc: "/pasos/paso3.png", // Ruta de la imagen de usuario con check
     title: "Generación del Negocio",
     description:
-      "Agregar una persona mas que sería la que ofrece la solución. Una vez confirmado el pago se envía la información del responsable de la demanda para que realicen el contacto directo de y respectiva negociación, detalles, precios, presupuestos, etc. La plataforma solo llevará estadisticas de conformidad entre la partes, no existiendo ningún otro costo adicional."
+      "Agregar una persona más que sería la que ofrece la solución. Corregir “ Una vez confirmado el pago del interesado para contactarse, se envía (la información) los datos del responsable de la demanda para que realicen el contacto directo de y respectiva negociación."
   }
 ];
 
@@ -28,9 +28,6 @@ export default function Pasos() {
       <h2 className="text-3xl font-semibold text-gray-800 mb-6"> {/* Título más grande */}
         CUAL ES EL FUNCIONAMIENTO DE  "NECESITO <span className="text-blue-700">ESTO!</span>"?
       </h2>
-      <p className="text-gray-600 mb-12"> {/* Más espacio entre párrafos */}
-        Detalla tu demanda de forma clara y precisa para que las personas interesadas puedan contactarse en forma directa y acercar una propuesta que sea la solución que estás buscando.
-      </p>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-12 max-w-6xl mx-auto">
         {pasos.map((paso) => (
           <div key={paso.id} className="flex flex-col items-center text-center">
