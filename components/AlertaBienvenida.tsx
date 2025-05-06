61fbdf0b15a29c9c15339629852de848f97d12ba
Agustin-Monti <153923960+Agustin-Monti@users.noreply.github.com>
2025-05-06 19:23:53 -0300

    Update AlertaBienvenida.tsx

--- components/AlertaBienvenida.tsx
+++ components/AlertaBienvenida.tsx
@@ -1,6 +1,9 @@
 // components/AlertaBienvenida.tsx
+
+"use client";
+
 import React from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 const AlertaBienvenida = ({ onClose }: { onClose: () => void }) => {
 
